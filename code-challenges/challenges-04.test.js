bc084b3f42a806ff638023d2bcd9a0d5224e8eda
SufianHamdan <sufian_hamdan@yahoo.com>
2021-04-29 22:28:36 +0300

    idontknow

--- code-challenges/challenges-04.test.js
+++ code-challenges/challenges-04.test.js
@@ -115,7 +115,7 @@ const alphabetizeBetter = (arr) => {
   function sortThings(a, b) {
     a = a.toLowerCase();
     b = b.toLowerCase();
-  
+
     if (a > b) {
       return 1;
     } else if (a < b) {
